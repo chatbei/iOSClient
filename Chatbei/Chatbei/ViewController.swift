abf27ad0a55618c018db3baaacd8f0c0dcb48166
chatbei <mac@macmatoMacBook-Pro.local>
2015-04-20 12:58:42 +0800

    Initial Commit

---
+++ Chatbei/Chatbei/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatbei
+//
+//  Created by tang on 15/4/20.
+//  Copyright (c) 2015年 tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
